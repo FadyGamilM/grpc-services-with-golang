b40b7e03c0abf1d52ec92bcf75338e870c5bafb3
[FadyGamilM] <1700961@eng.asu.edu.eg>
2023-12-12 05:43:03 +0200

    feature | finish the unary server

--- server/usermanager_server.go
+++ server/usermanager_server.go
@@ -3,8 +3,12 @@ package main
 import (
 	"context"
 	"log"
+	"math/rand"
+	"net"
+	"time"
 
 	pb "github.com/FadyGamilM/usermanagement/usermanager"
+	"google.golang.org/grpc"
 )
 
 const (
@@ -16,7 +20,34 @@ type UserManagerServer struct {
 	pb.UnimplementedUserManagerServer
 }
 
-func (ums *UserManagerServer) CreateUser(c context.Context, req pb.NewUser) (*pb.User, error) {
-	log.Printf("➜ new user to create : %v \n", req.UserInfo.Username)
-	return nil, nil
+func (ums *UserManagerServer) CreateUser(c context.Context, req *pb.NewUser) (*pb.User, error) {
+	log.Printf("[info] ➜ new user to create : %v \n", req.UserInfo.Username)
+	// define the user response and persist it in db and return the response
+	rand.Seed(time.Now().Unix())
+	userID := rand.Intn(1000000)
+	res := &pb.User{
+		UserId: int64(userID),
+		UserInfo: &pb.UserDetails{
+			Username: req.UserInfo.Username,
+			Age:      req.UserInfo.Age,
+		},
+	}
+	return res, nil
+}
+
+func main() {
+	// define a listener
+	listener, err := net.Listen("tcp", server_port)
+	if err != nil {
+		log.Fatalf("[X] ➜ failed to create a listener : %v \n", err)
+	}
+
+	server := grpc.NewServer()
+
+	// now register the server as a new grpc service and register our struct
+	pb.RegisterUserManagerServer(server, &UserManagerServer{})
+
+	if err := server.Serve(listener); err != nil {
+		log.Fatalf("[X] ➜ failed to start the server : %v \n", err)
+	}
 }
